13dae03b99113d92aaf30a5a7238a0d384a0a164
Berkay <berkayorhan@hotmail.se>
2026-06-08 15:57:05 +0200

    fix playwright bug

--- utils/supabase/session.ts
+++ utils/supabase/session.ts
@@ -8,28 +8,24 @@ export async function updateSession(request: NextRequest) {
   });
   const { key, url } = getSupabasePublicEnv();
 
-  const supabase = createServerClient(
-    url,
-    key,
-    {
-      cookies: {
-        getAll() {
-          return request.cookies.getAll();
-        },
-        setAll(cookiesToSet) {
-          for (const { name, value } of cookiesToSet) {
-            request.cookies.set(name, value);
-          }
-          supabaseResponse = NextResponse.next({
-            request,
-          });
-          for (const { name, value, options } of cookiesToSet) {
-            supabaseResponse.cookies.set(name, value, options);
-          }
-        },
+  const supabase = createServerClient(url, key, {
+    cookies: {
+      getAll() {
+        return request.cookies.getAll();
       },
-    }
-  );
+      setAll(cookiesToSet) {
+        for (const { name, value } of cookiesToSet) {
+          request.cookies.set(name, value);
+        }
+        supabaseResponse = NextResponse.next({
+          request,
+        });
+        for (const { name, value, options } of cookiesToSet) {
+          supabaseResponse.cookies.set(name, value, options);
+        }
+      },
+    },
+  });
 
   // refreshing the auth token
   await supabase.auth.getUser();
